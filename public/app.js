369050e6ddf2d69cbf93a0219d7b5405335fde6e
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-13 22:09:12 -0600

    Pressing scrape button should load articles now too

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
-$.getJSON("/articles", function(data) {
+const renderData = function(data) {
+
     // Declare article titles array
     var titles = [];
     // For each one
     for (var i = 0; i < data.length; i++) {
         // log
-        console.log("phew")
+        console.log('phew');
         // If it's not a duplicate
         if (!titles.includes(data[i].title)) {
             // Add the title to titles array
@@ -15,47 +16,100 @@ $.getJSON("/articles", function(data) {
             var id = data[i]._id;
             var title = data[i].title;
             // Display the apropos information on the page
-            $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
-            + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
-            + "<br /><br /><button class='save btn btn-light'>Save Article</button></p>");
+            $('.article-container').append(
+            "<p style='background-color:pink; padding: 10px; font-size: 20px'><a href=" +
+                link +
+                " data-id='" +
+                id +
+                "'><img style='width: 50%; height: 50%; padding: 10px'src=" +
+                img +
+                '><br />' +
+                title +
+                '</a>' +
+                "<br /><br /><button class='save btn btn-light'>Save Article</button></p>",
+            );
         }
     }
-});
+};
 
-$(document).on("click", ".scrape-new", function() {
+$(document).ready(function() {
+  $.get('/articles', renderData);
+  $(document).on('click', '.scrape-new', function() {
     // Empty the article-container div
-    $(".article-container").empty();
+    $('.article-container').empty();
     // First scrape
-    $.get("/scrape", function (response) {
-        // log data
-        // console.log(data);
-    }).then(function() {
-        // Grab the articles as a json
-        $.getJSON("/articles", function(data) {
-            // If we get data, change the alert div
-            $(".alert-h4").text("Scraping may take 15-20 seconds");
-            // Declare article titles array
-            var titles = [];
-            // For each one
-            for (var i = 0; i < data.length; i++) {
-                // log
-                console.log("phew")
-                // If it's not a duplicate
-                if (!titles.includes(data[i].title)) {
-                    // Add the title to titles array
-                    titles.push(data[i].title);
-                    // Save properties to variables b/c having trouble with them
-                    var link = data[i].link;
-                    var img = data[i].img;
-                    var id = data[i]._id;
-                    var title = data[i].title;
-                    // Display the apropos information on the page
-                    $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
-                    + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
-                    + "<br /><br /><button class='save btn btn-light'>Save Article</button></p>");
-                }
-            }
-        });
-    });
+      // Grab the articles as a json
+      // If we get data, then hide alert div
+      $('.alert-h4').text('Scraping may take 15-20 seconds');
+    $.get('/scrape', function(data) {
+      // log data
+      // console.log(data);
+      return data;
+    }).then(renderData);
+  })
 })
+
+
+
+
+// $.getJSON("/articles", function(data) {
+//     // Declare article titles array
+//     var titles = [];
+//     // For each one
+//     for (var i = 0; i < data.length; i++) {
+//         // log
+//         console.log("phew")
+//         // If it's not a duplicate
+//         if (!titles.includes(data[i].title)) {
+//             // Add the title to titles array
+//             titles.push(data[i].title);
+//             // Save properties to variables b/c having trouble with them
+//             var link = data[i].link;
+//             var img = data[i].img;
+//             var id = data[i]._id;
+//             var title = data[i].title;
+//             // Display the apropos information on the page
+//             $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
+//             + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
+//             + "<br /><br /><button class='save btn btn-light'>Save Article</button></p>");
+//         }
+//     }
+// });
+
+// $(document).on("click", ".scrape-new", function() {
+//     // Empty the article-container div
+//     $(".article-container").empty();
+//     // First scrape
+//     $.get("/scrape", function (response) {
+//         // log data
+//         // console.log(data);
+//     }).then(function() {
+//         // Grab the articles as a json
+//         $.getJSON("/articles", function(data) {
+//             // If we get data, change the alert div
+//             $(".alert-h4").text("Scraping may take 15-20 seconds");
+//             // Declare article titles array
+//             var titles = [];
+//             // For each one
+//             for (var i = 0; i < data.length; i++) {
+//                 // log
+//                 console.log("phew")
+//                 // If it's not a duplicate
+//                 if (!titles.includes(data[i].title)) {
+//                     // Add the title to titles array
+//                     titles.push(data[i].title);
+//                     // Save properties to variables b/c having trouble with them
+//                     var link = data[i].link;
+//                     var img = data[i].img;
+//                     var id = data[i]._id;
+//                     var title = data[i].title;
+//                     // Display the apropos information on the page
+//                     $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
+//                     + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
+//                     + "<br /><br /><button class='save btn btn-light'>Save Article</button></p>");
+//                 }
+//             }
+//         });
+//     });
+// })
   
--- server.js
+++ server.js
@@ -1,75 +1,82 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+ 
+ // Our scraping tools
+ // Axios is a promisedbased http library, similar to jQuery's Ajax method
+ // It works on the client and on the server
+var axios = require('axios');
+var cheerio = require('cheerio');
+ 
+ // Require all models
+var db = require('./models');
+ 
 var PORT = process.env.PORT || 3000;
-
-// Initialize Express
+ 
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
+ // Configure middleware
+ 
+ // Use morgan logger for logging requests
+app.use(logger('dev'));
+ // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
+ // Make public a static folder
 app.use(express.static("public"));
+ 
+ // Connect to the Mongo DB
+mongoose.connect(
+  'mongodb://localhost/mongoHeadlines',
+  {useNewUrlParser: true},
+);
+ 
+ // Routes
+ 
+ // A GET route for scraping the LiveKindly website
+app.get('/scrape', function(req, res) {
+   // First, we grab the body of the html with axios
+  axios.get('https://www.livekindly.co/').then(function(response) {
+     // Then, we load that into cheerio and save it to $ for a shorthand selector
+     var $ = cheerio.load(response.data);
+ 
+     // Now, we grab every h2 within an article tag, and do the following:
+    var articles = [];
+    $('.td-image-wrap').each(function(i, element) {
+       // Save an empty result object
+       var result = {};
+       // Add the text, href and summary of every article, and save them as properties of the result object
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
-
-// Routes
-
-// A GET route for scraping the LiveKindly website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.livekindly.co/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $(".td-image-wrap").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-      // Add the text, href and summary of every article, and save them as properties of the result object
-      result.link = $(this)
-        .attr("href");
-      result.title = $(this)
-        .attr("title");
-      // Get text containing img src
-      var img = $(this).children("span").attr("style");
-      // Dissect src
+      result.link = $(this).attr('href');
+      result.title = $(this).attr('title');
+       // Get text containing img src
+      var img = $(this)
+        .children('span')
+        .attr('style');
+       // Dissect src
       img = img.split("(")[1];
       img = img.split(")")[0];
-      // Set result img property
-      result.img = img;
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-      .then(function(dbArticle) {
-        // View the added result in the console
-        console.log(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, log it
-        console.log(err);
-      });
-    });
-  })
-  // Send a message to the client
-  res.send("Scrape Complete");
-});
 
+      const newArticle = db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+          return dbArticle;
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+      articles.push(newArticle);
+     });
+
+    Promise.all(articles).then(data => {
+      res.json(data);
+    });
+  });
+   // Send a message to the client
+ });
+ 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
@@ -150,7 +157,7 @@ app.post("/comment/:id", function (req, res) {
   db.Comment.create(req.body)
       .then(function (dbComment) {
           // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
-          // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+          // { new: true } tells the query that we want it to return the updated User  it returns the original by default
           // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
           return db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { comment: dbComment._id }}, { new: true });
       })
@@ -183,5 +190,5 @@ app.delete("/comment/:id", function (req, res) {
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("App running on port "  + PORT + "!");
 });
