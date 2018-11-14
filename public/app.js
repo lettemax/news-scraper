49cd6fec4b7f4dd6929d69725f1fc40467f2a25a
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-13 20:38:08 -0600

    Trouble getting elements to display on same click as scrape

--- public/app.js
+++ public/app.js
@@ -1,83 +1,37 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // If we get data, then get rid of orange message div
-    $(".alert").hide();
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Save properties to variables b/c having trouble with them
-      var link = data[i].link;
-      var img = data[i].img;
-      var id = data[i]._id;
-      var title = data[i].title;
-      // Display the apropos information on the page
-      $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
-       + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
-       + "<br /><br /><button>Save Article</button></p>");
-    }
-  });
-  
-  
-  
-//   // Whenever someone clicks a p tag
-//   $(document).on("click", "p", function() {
-//     // Empty the notes from the note section
-//     $("#notes").empty();
-//     // Save the id from the p tag
-//     var thisId = $(this).attr("data-id");
-  
-//     // Now make an ajax call for the Article
-//     $.ajax({
-//       method: "GET",
-//       url: "/articles/" + thisId
-//     })
-//       // With that done, add the note information to the page
-//       .then(function(data) {
-//         console.log(data);
-//         // The title of the article
-//         $("#notes").append("<h2>" + data.title + "</h2>");
-//         // An input to enter a new title
-//         $("#notes").append("<input id='titleinput' name='title' >");
-//         // A textarea to add a new note body
-//         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-//         // A button to submit a new note, with the id of the article saved to it
-//         $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-//         // If there's a note in the article
-//         if (data.note) {
-//           // Place the title of the note in the title input
-//           $("#titleinput").val(data.note.title);
-//           // Place the body of the note in the body textarea
-//           $("#bodyinput").val(data.note.body);
-//         }
-//       });
-//   });
-  
-//   // When you click the savenote button
-//   $(document).on("click", "#savenote", function() {
-//     // Grab the id associated with the article from the submit button
-//     var thisId = $(this).attr("data-id");
-  
-//     // Run a POST request to change the note, using what's entered in the inputs
-//     $.ajax({
-//       method: "POST",
-//       url: "/articles/" + thisId,
-//       data: {
-//         // Value taken from title input
-//         title: $("#titleinput").val(),
-//         // Value taken from note textarea
-//         body: $("#bodyinput").val()
-//       }
-//     })
-//       // With that done
-//       .then(function(data) {
-//         // Log the response
-//         console.log(data);
-//         // Empty the notes section
-//         $("#notes").empty();
-//       });
-  
-//     // Also, remove the values entered in the input and textarea for note entry
-//     $("#titleinput").val("");
-//     $("#bodyinput").val("");
-//   });
+$(document).on("click", ".scrape-new", function() {
+    // Empty the article-container div
+    $(".article-container").empty();
+    // First scrape
+    $.get("/scrape", function (data) {
+        // log data
+        // console.log(data);
+    }).then(function() {
+        // Grab the articles as a json
+        $.getJSON("/articles", function(data) {
+            // If we get data, then hide alert div
+            $(".alert-h4").text("Scraping may take 15-20 seconds");
+            // Declare article titles array
+            var titles = [];
+            // For each one
+            for (var i = 0; i < data.length; i++) {
+                // log
+                console.log("phew")
+                // If it's not a duplicate
+                if (!titles.includes(data[i].title)) {
+                    // Add the title to titles array
+                    titles.push(data[i].title);
+                    // Save properties to variables b/c having trouble with them
+                    var link = data[i].link;
+                    var img = data[i].img;
+                    var id = data[i]._id;
+                    var title = data[i].title;
+                    // Display the apropos information on the page
+                    $(".article-container").append("<p style='background-color:pink; padding: 10px; font-size: 20px'><a href="
+                    + link + " data-id='" + id + "'><img style='width: 50%; height: 50%; padding: 10px'src=" + img + "><br />" + title + "</a>"
+                    + "<br /><br /><button class='save btn btn-light'>Save Article</button></p>");
+                }
+            }
+        });
+    });
+})
   
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -54,7 +54,6 @@ app.get("/scrape", function(req, res) {
       img = img.split(")")[0];
       // Set result img property
       result.img = img;
-
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
       .then(function(dbArticle) {
@@ -65,7 +64,6 @@ app.get("/scrape", function(req, res) {
         // If an error occurred, log it
         console.log(err);
       });
-
     });
   })
   // Send a message to the client
@@ -86,41 +84,6 @@ app.get("/articles", function(req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
 
 // Start the server
 app.listen(PORT, function() {
