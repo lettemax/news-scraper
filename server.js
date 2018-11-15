53b1c5a0de5117681b6ba2c4194cd769f2ff023c
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-14 23:30:23 -0600

    Heroku addon config

--- server.js
+++ server.js
@@ -26,10 +26,11 @@ app.use(express.json());
 app.use(express.static("public"));
  
  // Connect to the Mongo DB
-mongoose.connect(
-  'mongodb://localhost/mongoHeadlines',
-  {useNewUrlParser: true},
-);
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
  
  // Routes
  
